bd7c3baac863c1c1e9dccf04bd2435f091d76624
Smit Pawar <dynaster21@gmail.com>
2025-09-24 16:47:38 +0530

    initial setup completed

---
+++ finDock/src/main/java/com/smit/finDock/FinDockApplication.java
@@ -0,0 +1,13 @@
+package com.smit.finDock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinDockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinDockApplication.class, args);
+	}
+
+}
---
+++ finDock/src/test/java/com/smit/finDock/FinDockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smit.finDock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinDockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
